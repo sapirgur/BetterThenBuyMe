9d7aae2f07309774c7aa7ce34c29497d16db0e13
tom123996 <tomgrvch123996@gmail.com>
2024-07-31 22:39:26 +0300

    did npm install passport, added the facebook post feature to this branch from the facebookPost- feature-fixed, changed footer cnd it's css to match the facebook feature. added facebookPost.js file with javascript logic for the facebook posts

---
+++ Public/js/facebookPost.js
@@ -0,0 +1,24 @@
+document.getElementById('postButton').addEventListener('click', function() { // the event listener that "captures" the click on the button
+
+    const messageInput = document.getElementById('messageInput'); // gets the input by id
+    const message = messageInput.value.trim(); //removes white space from both ends of the message
+
+    if (message === '') { // checks if input content is empty
+        alert('Please enter a message.');
+        return;
+    }
+
+    fetch(`http://localhost:3001/postToFacebook`, { //sends a fetch request to server with the message as the request body
+        method: 'POST', // specifies the http method as post 
+        headers: {
+            'Content-Type': 'application/json' // indicates that the request body contains json
+        },
+        body: JSON.stringify({ message: message }) // converts the message to a json string
+    })
+    .then(response => response.text()) // processes the HTTP response from the server
+    .then(data => { // handles the text data obtained from the previous ".then()"
+        alert(data); // alert to show on page if succeeded or not or if logged in or not
+        messageInput.value = '';
+    })
+    .catch(error => console.error('Error:', error)); // catches and logs the error to the browser console
+});
